8a24d7a95ff213a10ad6ee0851fb565f6eacb1d5
Tomas D <tomasduron1@gmail.com>
2023-11-09 20:41:15 -0800

    IT BROKE

--- main-page/js/main.js
+++ main-page/js/main.js
@@ -1,12 +1,12 @@
-// var xhttp = new XMLHttpRequest();
-// xhttp.open("GET", "https://github.com/Tomas-Duron/Tomas-Duron.github.io/blob/main/styles.css", true);
-// xhttp.onreadystatechange = function() {
-//   if (xhttp.readyState === 4) {
-//     if (xhttp.status === 200) {
-//       var link = document.createElement('style');
-// link.innerHTML=xhttp.responseText;
-// document.getElementsByTagName('head')[0].appendChild(link);
-//     }
-//   }
-// }
-// xhttp.send(null);
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://github.com/Tomas-Duron/Tomas-Duron.github.io/blob/main/styles.css", true);
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState === 4) {
+    if (xhttp.status === 200) {
+      var link = document.createElement('style');
+link.innerHTML=xhttp.responseText;
+document.getElementsByTagName('head')[0].appendChild(link);
+    }
+  }
+}
+xhttp.send(null);
